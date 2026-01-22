4e585f9072405dc3f020d390824b8f7ca4704540
MJD19994 <mttdudley@gmail.com>
2026-01-21 19:19:33 -0500

    Update install.sh to fix kernel headers package error on Trixie.

--- install.sh
+++ install.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Install script for WM8960 AudioHAT Drivers
+# Check for root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
-# Update package list
+# Update package lists
 apt-get update
 
-# Install necessary packages
+# Install required packages
 apt-get -y install linux-headers-$(uname -r)
-# other installation commands...
+apt-get -y install build-essential
+
+# Clone the repository
+if [ ! -d "WM8960_AudioHAT_Drivers" ]; then
+    git clone https://github.com/MJD19994/WM8960_AudioHAT_Drivers.git
+fi
+
+# Build the drivers
+cd WM8960_AudioHAT_Drivers
+make
+
+# Install the drivers
+make install
+
+echo "Installation complete!"
